8c70836e71691161607460f31b525f9aff412114
R. S. Doiel <rsdoiel@usc.edu>
2012-09-18 16:06:47 -0700

    Fixed comparison for now/today to return current date.

--- usc.js
+++ usc.js
@@ -27,8 +27,8 @@
             }
 
             if (date_string.match(reRelativeDate)) {
-                if (date_string.toLowerCase("now") === true ||
-                        date_string.toLowerCase("today") === true) {
+                if (date_string.toLowerCase() === "now" ||
+                        date_string.toLowerCase() === "today") {
                     return now;
                 } else if (date_string.indexOf(" ") < 0) {
                     throw "Not a relative date string:" + date_string;
