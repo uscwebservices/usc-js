5334db9d9e994fe870471bcf9feef35ed8714832
R. S. Doiel <rsdoiel@gmail.com>
2012-09-17 22:00:58 -0700

    Added test for sqlDateTime(), relativeDateTime(), sqlDate()

--- tests/usc_test.js
+++ tests/usc_test.js
@@ -13,30 +13,138 @@ var path = require("path"),
     harness = require("../lib/harness"),
     USC = require("../usc").USC;
 
+// Test object structure
 harness.push({callback: function () {
-    var test_date = new Date("09/17/2012"),
-        test_date2 = new Date(),
-        s,
-        expected_s,
-        lzero = function (s) {
-            s = "0" + s;
-            return s.substr(s.length - 2);
-        };
-
     assert.ok(USC, "Should have a USC object create.");
+    assert.equal(typeof USC.sqlDateTime, "function", "Should have a sqlDateTime method");
+    assert.ok(typeof USC.relativeDateTime, "function", "Should have a relativeDateTime method");
     assert.equal(typeof USC.sqlDate, "function", "Should have a sqlDate method");
-    assert.ok(typeof USC.relativeDate, "function", "Should have a relativeDate method");
+}, label: "Test object structure"});
 
-    s = USC.sqlDate(test_date);
-    expected_s = "2012-09-17";
-    assert.equal(s, expected_s, "Should have toYYYYMMDD() date.");
+// Test sqlDateTime(), relativeDateTime()
+harness.push({callback: function () {
+    var today = new Date(),
+        test_date1 = new Date(),
+        test_date2 = new Date(),
+        YY,
+        MM,
+        DD,
+        hh,
+        mm,
+        ss,
+        s,
+        expected_s;
+
+    YY = today.getFullYear();
+    MM = "0" + (today.getMonth() + 1);
+    DD = "0" + today.getDate();
+    hh = "0" + today.getHours();
+    mm = "0" + today.getMinutes();
+    ss = "0" + today.getSeconds();
+
+    MM = MM.substr(MM.length - 2);
+    DD = DD.substr(DD.length - 2);
+    hh = hh.substr(hh.length - 2);
+    mm = mm.substr(mm.length - 2);
+    ss = ss.substr(ss.length - 2);
+
+    s = USC.sqlDateTime(test_date1);
+    expected_s = [
+        YY, "-", MM, "-", DD, " ",
+        hh, ":", mm, ":", ss
+    ].join("");
+    assert.equal(s, expected_s, "\n" + s + "\n" + expected_s);
 
     test_date2.setYear(test_date2.getFullYear() + 1);
-    s = USC.sqlDate("+1 year");
-    expected_s = test_date2.getFullYear() + "-" + lzero(1 + test_date2.getMonth()) + "-" + test_date2.getDate();
+    YY = test_date2.getFullYear();
+    MM = "0" + (test_date2.getMonth() + 1);
+    DD = "0" + test_date2.getDate();
+    hh = "0" + test_date2.getHours();
+    mm = "0" + test_date2.getMinutes();
+    ss = "0" + test_date2.getSeconds();
 
-    assert.equal(s, expected_s, "toYYYYMMDD() should render relative year date.");
-}, label: "Test date methods on the USC object."});
+    MM = MM.substr(MM.length - 2);
+    DD = DD.substr(DD.length - 2);
+    hh = hh.substr(hh.length - 2);
+    mm = mm.substr(mm.length - 2);
+    ss = ss.substr(ss.length - 2);
+
+    s = USC.sqlDateTime("+1 year", false, test_date1);
+    expected_s = [
+        YY, "-", MM, "-", DD, " ",
+        hh, ":", mm, ":", ss
+    ].join("");
+    assert.equal(s, expected_s, "\n" + s + "\n" + expected_s);
+}, label: "Test sqlDateTime methods on the USC object."});
+
+// Test sqlDate()
+harness.push({callback: function () {
+    var today = new Date(),
+        test_date1 = new Date(),
+        test_date2 = new Date(),
+        YY,
+        MM,
+        DD,
+        s,
+        expected_s;
+
+    YY = today.getFullYear();
+    MM = "0" + (today.getMonth() + 1);
+    DD = "0" + today.getDate();
+
+    MM = MM.substr(MM.length - 2);
+    DD = DD.substr(DD.length - 2);
+
+    s = USC.sqlDate(test_date1);
+    expected_s = [
+        YY, "-", MM, "-", DD
+    ].join("");
+    assert.equal(s, expected_s, "\n" + s + "\n" + expected_s);
+
+    test_date2.setYear(test_date2.getFullYear() + 1);
+    YY = test_date2.getFullYear();
+    MM = "0" + (test_date2.getMonth() + 1);
+    DD = "0" + test_date2.getDate();
+
+    MM = MM.substr(MM.length - 2);
+    DD = DD.substr(DD.length - 2);
+
+    s = USC.sqlDate("+1 year", false, test_date1);
+    expected_s = [
+        YY, "-", MM, "-", DD
+    ].join("");
+    assert.equal(s, expected_s, "\n" + s + "\n" + expected_s);
+
+    test_date1 = new Date();
+    test_date1.setYear(test_date1.getFullYear() + 1);
+    YY = test_date1.getFullYear();
+    MM = "0" + (test_date1.getMonth() + 1);
+    DD = "0" + test_date1.getDate();
+
+    MM = MM.substr(MM.length - 2);
+    DD = DD.substr(DD.length - 2);
+
+    s = USC.sqlDate(test_date1);
+    expected_s = [
+        YY, "-", MM, "-", DD
+    ].join("");
+    assert.equal(s, expected_s, "\n" + s + "\n" + expected_s);
+
+    test_date2 = new Date();
+    test_date2.setYear(test_date1.getFullYear() + 1);
+    YY = test_date2.getFullYear();
+    MM = "0" + (test_date2.getMonth() + 1);
+    DD = "0" + test_date2.getDate();
+
+    MM = MM.substr(MM.length - 2);
+    DD = DD.substr(DD.length - 2);
+
+    s = USC.sqlDate("+1 year", false, test_date1);
+    expected_s = [
+        YY, "-", MM, "-", DD
+    ].join("");
+    assert.equal(s, expected_s, "\n" + s + "\n" + expected_s);
+}, label: "Test sqlDate() methods on the USC object."});
 
 if (require.main === module) {
     harness.RunIt(path.basename(module.filename), 10, true);
--- usc.js
+++ usc.js
@@ -7,87 +7,205 @@
 // Version 0.0.1 (Sept. 2012)
 //
 /*jslint devel: true, node: true, maxerr: 50, indent: 4,  vars: true, sloppy: true */
+(function (global) {
+    var USC = {
+        relativeDateTime: function (date_string, seedDate) {
+            var reRelativeDate = /now|[\-+0-9]+\s+(day|month|year)/i,
+                toks,
+                offset,
+                unit,
+                now;
 
-var USC = {
-    relativeDate: function (date_string, seedDate) {
-        var reRelativeDate = /now|[\-+0-9]+\s+(day|month|year)/i,
-            toks,
-            offset,
-            unit,
-            now;
+            if (seedDate === undefined) {
+                now = new Date();
+            } else if (seedDate instanceof Date) {
+                now = seedDate;
+            }
 
-	if (seedDate === undefined) {
-            now = new Date();
-	} else if (seedDate instanceof Date) {
-	    now = seedDate;
-	}
-    
-        if (date_string === undefined) {
-            date_string = "";
-        }
-    
-        if (date_string.match(reRelativeDate)) {
-            if (date_string.toLowerCase("now") === true || date_string.toLowerCase("today") === true) {
+            if (date_string === undefined) {
+                date_string = "";
+            }
+
+            if (date_string.match(reRelativeDate)) {
+                if (date_string.toLowerCase("now") === true ||
+                        date_string.toLowerCase("today") === true) {
+                    return now;
+                }
+                toks = date_string.split(/\s+/, 2);
+                offset = Number(toks[0]);
+                unit = toks[1].toLowerCase().trim();
+                switch (unit) {
+                case 'years':
+                case 'year':
+                case 'yr':
+                case 'y':
+                    offset += now.getFullYear();
+                    now.setYear(offset);
+                    break;
+                case 'months':
+                case 'month':
+                case 'mon':
+                case 'mo':
+                case 'm':
+                    offset += now.getMonth();
+                    now.setMonth(offset);
+                    break;
+                case 'days':
+                case 'day':
+                case 'dy':
+                case 'd':
+                    offset += now.getDate();
+                    now.setDate(offset);
+                    break;
+                case 'hours':
+                case 'hour':
+                case 'hr':
+                case 'h':
+                    offset += now.getHours();
+                    now.setHours(offset);
+                    break;
+                case 'minutes':
+                case 'minute':
+                case 'min':
+                case 'mn':
+                case 'i':
+                    offset += now.getMinutes();
+                    now.setMinutes(offset);
+                    break;
+                case 'seconds':
+                case 'second':
+                case 'sec':
+                case 's':
+                    offset += now.getSeconds();
+                    now.setSeconds(offset);
+                    break;
+                }
                 return now;
             }
-            toks = date_string.split(/\s+/, 2);
-            offset = Number(toks[0]);
-            unit = toks[1].toLowerCase().trim();
-            switch (unit) {
-            case 'year':
-            case 'years':
-                // Fixme handle leap years.                
-                offset += now.getFullYear();
-                now.setYear(offset);
-                break;
-            case 'month':
-            case 'months':
-                offset += now.getMonth();
-                now.setMonth(offset);
-                break;
-            case 'day':
-            case 'days':
-                offset += now.getDate();
-                now.setDate(offset);
-                break;
-            }
-            return now;
-        }
-        return new Date(date_string);
-    },
+            return new Date(date_string);
+        },
 
-    sqlDate: function (dateObj, use_UTC) {
-        var dt = {
-            fromDate: function (dateObj, use_UTC) {
-                if (use_UTC === undefined ||
-                        use_UTC === false) {
-                    this.YYYY = dateObj.getFullYear();
-                    this.MM = (dateObj.getMonth() + 1);
-                    this.DD =  dateObj.getDate();
-                } else if (use_UTC === true) {
-                    this.YYYY = dateObj.getUTCFullYear();
-                    this.MM = dateObj.getUTCMonth() + 1;
-                    this.DD = dateObj.getUTCDate();
+        sqlDate: function (dateObj, use_UTC, seedDate) {
+            var dt;
+
+            dt = {
+                fromDate: function (dateObj, use_UTC) {
+                    if (use_UTC === undefined ||
+                            use_UTC === false) {
+                        this.YYYY = dateObj.getFullYear();
+                        this.MM = (dateObj.getMonth() + 1);
+                        this.DD = dateObj.getDate();
+                    } else if (use_UTC === true) {
+                        this.YYYY = dateObj.getUTCFullYear();
+                        this.MM = dateObj.getUTCMonth() + 1;
+                        this.DD = dateObj.getUTCDate();
+                    }
+                },
+                toString: function () {
+                    var MM = "0" + this.MM,
+                        DD = "0" + this.DD;
+                    MM = MM.substr(MM.length - 2);
+                    DD = DD.substr(DD.length - 2);
+                    return [this.YYYY, MM, DD].join("-");
                 }
-            },
-            toString: function () {
-                var mm = "0" + this.MM,
-                    dd = "0" + this.DD;
-                mm = mm.substr(mm.length - 2);
-                dd = dd.substr(dd.length - 2);
-                return [this.YYYY, mm, dd].join("-");
+            };
+
+            if (dateObj instanceof Date) {
+                dt.fromDate(dateObj, use_UTC);
+            } else if (seedDate === undefined) {
+                dt.fromDate(this.relativeDateTime(dateObj));
+            } else {
+                dt.fromDate(this.relativeDateTime(dateObj, seedDate));
             }
-        };
+            return dt.toString();
+        },
 
-        if (dateObj instanceof Date) {
-            dt.fromDate(dateObj, use_UTC);
-        } else {
-            dt.fromDate(this.relativeDate(dateObj));
+        sqlDateTime: function (dateObj, use_UTC, seedDate) {
+            var dt;
+
+            dt = {
+                fromDate: function (dateObj, use_UTC) {
+                    if (use_UTC === undefined ||
+                            use_UTC === false) {
+                        this.YYYY = dateObj.getFullYear();
+                        this.MM = (dateObj.getMonth() + 1);
+                        this.DD = dateObj.getDate();
+                        this.hh = dateObj.getHours();
+                        this.mm = dateObj.getMinutes();
+                        this.ss = dateObj.getSeconds();
+                    } else if (use_UTC === true) {
+                        this.YYYY = dateObj.getUTCFullYear();
+                        this.MM = dateObj.getUTCMonth() + 1;
+                        this.DD = dateObj.getUTCDate();
+                        this.hh = dateObj.getUTCHours();
+                        this.mm = dateObj.getUTCMinutes();
+                        this.ss = dateObj.getUTCSeconds();
+                    }
+                },
+                toString: function () {
+                    var MM = "0" + this.MM,
+                        DD = "0" + this.DD,
+                        hh = "0" + this.hh,
+                        mm = "0" + this.mm,
+                        ss = "0" + this.ss;
+                    MM = MM.substr(MM.length - 2);
+                    DD = DD.substr(DD.length - 2);
+                    hh = hh.substr(hh.length - 2);
+                    mm = mm.substr(mm.length - 2);
+                    ss = ss.substr(ss.length - 2);
+                    return [this.YYYY, MM, DD].join("-") +
+                        " " + [hh, mm, ss].join(":");
+                }
+            };
+
+            if (dateObj instanceof Date) {
+                dt.fromDate(dateObj, use_UTC);
+            } else if (seedDate === undefined) {
+                dt.fromDate(this.relativeDateTime(dateObj));
+            } else {
+                dt.fromDate(this.relativeDateTime(dateObj, seedDate));
+            }
+            return dt.toString();
         }
-        return dt.toString();
-    }
-};
+    };
 
-if (exports !== undefined) {
-    exports.USC = USC;
-}
+    // If we're in NodeJS get content via http.Client
+    // If we're running in the browser get content via XHR
+    if (global.window === undefined) {
+        (function (global) {
+            var http = require("http");
+
+            global.getJSON = function (url, callback) {
+                http.get(url, function (res) {
+                    console.log("Got response: " + res);
+                    callback(res.data);
+                }).on('error', function (e) {
+                    console.error("Got error: " + e.message);
+                    throw "Got error: " + e.message;
+                });
+            };
+        }(global));
+    } else {
+        (function (global) {
+            var httpRequest;
+
+            if (window.XMLHttpRequest === undefined) {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            } else {
+                httpRequest = new XMLHttpRequest();
+            }
+
+            global.getJSON = function (url, callback) {
+                httpRequest.onreadystatechange = callback;
+                httpRequest.open('GET', url, true);
+                httpRequest.send(null);
+            };
+        }(global));
+    }
+        
+    global.USC = USC;
+    if (global.exports === undefined) {
+        global.exports = {};
+    }
+    global.exports.USC = USC;
+}(this));
